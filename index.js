ba3c366f5375ed4e0d13349bd2317dd72670bd98
winterprogram <chakladar.sandeep4@gmail.com>
2020-03-01 15:59:13 +0530

    First commit of website

---
+++ index.js
@@ -0,0 +1,15 @@
+$('document').ready(() => {
+   
+
+    $("#shop").dblclick(() => {
+        alert("Enter your Shop name as per Gov license")
+    });
+
+    $("#hide").hover(function () {
+        $(this).hide("1000")
+    })
+
+
+
+
+})
