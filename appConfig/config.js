c270da2e97e4c51f3065b6d4ce10367f381206fc
winterprogram <chakladar.sandeep4@gmail.com>
2020-04-25 16:42:39 +0530

    mog uri

--- appConfig/config.js
+++ appConfig/config.js
@@ -3,12 +3,12 @@ let appconfig = {}
 appconfig.port = 3000;
 // appconfig.Crosorigin = "http://localhost:4200/";
 appconfig.env = "dev";
-// appconfig.db = {
-//     uri :"mongodb://127.0.0.1:27017/mains"
-// };
-appconfig.db={
-    uri:`mongodb+srv://servertest:examidea123@cluster0-lbhqk.mongodb.net/test?retryWrites=true&w=majority`
-}
+appconfig.db = {
+    uri :"mongodb://127.0.0.1:27017/mains"
+};
+// appconfig.db={
+//     uri:`mongodb+srv://servertest:examidea123@cluster0-lbhqk.mongodb.net/test?retryWrites=true&w=majority`
+// }
 appconfig.version = "0.0.1"
 
 module.exports = {
