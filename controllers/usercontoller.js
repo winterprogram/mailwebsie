8354739a4eb797eb13b1279df82adb8a85aaab76
winterprogram <chakladar.sandeep4@gmail.com>
2020-05-11 02:04:45 +0530

    adding stage - 3 for coupon distubition to user from merchant if the user is new in db

--- controllers/usercontoller.js
+++ controllers/usercontoller.js
@@ -24,6 +24,10 @@ const distance = require('google-distance-matrix');
 const geolib = require('geolib');
 const c = require('./../models/Coupongen')
 const coupon = mongoose.model('coupons')
+const a = require('./../models/UserCoupon')
+const userCoupon = mongoose.model('coupondistribution')
+const signup = mongoose.model('signupforuser')
+const moment = require('moment')
 
 let userMerchantDisplay = (req, res) => {
 
@@ -121,6 +125,7 @@ let userCouponDisribution = (req, res) => {
                         "discount": 1,
                         "faltdiscountupto": 1,
                         "valid": 1,
+                        "category": "$geocode.category",
                         "latitude": "$geocode.latitude",
                         "longitude": "$geocode.longitude"
                     }
@@ -157,20 +162,289 @@ let userCouponDisribution = (req, res) => {
                 }
             }
             if (validCoupon.length != 0) {
-                logger.info('merchant >0 available with valid coupon', 'findAllActiveCoupon:userCouponDisribution()')
-                console.log(validCoupon)
+                logger.info('merchant > 0 available with valid coupon', 'findAllActiveCoupon:userCouponDisribution()')
+                // console.log(validCoupon)
                 resolve(validCoupon)
             } else {
-                console.log(nonValidcoupon)
-                logger.error('all merchant\'s coupon are valid =0', 'findValidCoupon:userCouponDisribution()', 1)
-                let response = api.apiresponse(true, 404, 'all merchant\'s coupon are valid =0', nonValidcoupon)
+                // console.log(nonValidcoupon)
+                logger.error('all merchant\'s coupon are valid = 0', 'findValidCoupon:userCouponDisribution()', 1)
+                let response = api.apiresponse(true, 404, 'all merchant\'s coupon are valid = 0', nonValidcoupon)
                 reject(response)
             }
         })
     }
 
+    let findCouponNeartoUser = (validCoupon) => {
+        return new Promise((resolve, reject) => {
+            // console.log(validCoupon)
+            logger.info('user found', 'finduser:userCouponDisribution()')
+            // console.log(resultData)
+            // resolve(resultData)
+            let listOfAllCouponWithInRangeTwo = [];
+            // let listOfAllCouponWithInRangeFive = []
+            let userlatitude = "18.995279";
+            let userlongitude = "73.116272";
+            for (let i in validCoupon) {
+                // console.log(validCoupon)
+                logger.info('caculating user to coupon distance', 'merchantToUser:userCouponDisribution()')
+                let userdistance = (geolib.getDistance(
+                    { latitude: Number(userlatitude), longitude: Number(userlongitude) },
+                    { latitude: Number(validCoupon[i].latitude), longitude: Number(validCoupon[i].longitude) }
+                )) * 0.001
+                // console.log(userdistance)
+                if (userdistance <= 5) {
+                    logger.info('coupon found with in 5KM radius', 'merchantToUser:userCouponDisribution()')
+                    listOfAllCouponWithInRangeTwo.push(validCoupon[i])
+                }
+                //  else if (userdistance <= 5) {
+                //     logger.info('coupon found with in 5KM radius', 'merchantToUser:userCouponDisribution()')
+                //     listOfAllCouponWithInRangeFive.push(validCoupon[i])
+                // }
+            }
+            if (listOfAllCouponWithInRangeTwo.length != 0) {
+                logger.info('coupon found with in 2KM radius -2', 'merchantToUser:userCouponDisribution()')
+                // console.log(listOfAllCouponWithInRangeTwo)
+                resolve(listOfAllCouponWithInRangeTwo)
+            } else {
+                logger.error('no coupon found with in 5KM', 'merchantToUser:userCouponDisribution()', 5)
+                reject(listOfAllCouponWithInRangeTwo)
+            }
 
-    addGeoCode(req, res).then(findValidCoupon).then((resolve) => {
+            // }
+        })
+
+    }
+
+    let sortCouponToUsercategory = (listOfAllCouponWithInRangeTwo) => {
+        return new Promise((resolve, reject) => {
+            signup.find({ userid: "TidWCS" }).lean().exec((err, resultData) => {
+                // console.log(resultData[0].categoryselected:) 
+                if (err) {
+                    logger.error('error while fetching user with userid', 'finduser:userCouponDisribution()', 5)
+                    let response = api.apiresponse(true, 403, 'error while fetching user with userid', null)
+                    reject(response)
+                } else if (emptyCheck.emptyCheck(resultData)) {
+                    logger.error('No user found with userid', 'finduser:userCouponDisribution()', 1)
+                    let response = api.apiresponse(true, 404, 'No user found with userid', null)
+                    reject(response)
+                } else {
+                    let category = resultData[0].categoryselected;
+                    //sorting the array of merchant coupon depending on user category list priority
+                    let sortcategorylist = listOfAllCouponWithInRangeTwo.filter(i => category.includes(i.category))
+                    sortcategorylist.sort()
+                    resolve([category, sortcategorylist])
+                }
+            })
+
+        })
+    }
+    let couponToUser = ([category, sortcategorylist]) => {
+        return new Promise((resolve, reject) => {
+            // console.log(sortcategorylist)
+            let firstuserCoupon = [];
+            let userCategory = category;
+            let couponSortList = sortcategorylist;
+            // sort by priority
+            couponSortList.sort(function (c = couponSortList, d = userCategory) {
+                for (let i in d) {
+                    let Ac = c.category;
+                    let Bc = d[i];
+                    if (Ac < Bc) {
+                        return -1;
+                    }
+                }
+            })
+            for (let i in userCategory) {
+                let couponDist = couponSortList.find(o => o.category == userCategory[i]);
+                if (couponDist != undefined) {
+                    firstuserCoupon.push(couponDist)
+                    console.log(firstuserCoupon)
+                }
+            }
+
+            // console.log(couponSortList)
+            // for (let i in userCategory) {
+            userCoupon.find({ userid: "TidWCS" }).lean().exec((err, data) => {
+                console.log(data)
+                if (err) {
+                    logger.error('error while fetching user with userid and category', 'couponToUser:userCouponDisribution()', 5)
+                    let response = api.apiresponse(true, 403, 'error while fetching user with userid and category', null)
+                    reject(response)
+                } else if (emptyCheck.emptyCheck(data)) {
+                    // console.log(data)
+                    console.log(emptyCheck.emptyCheck(data))
+                    // add header params here
+                    let purchasedAmount = 1200;
+                    if (( purchasedAmount < 500 && purchasedAmount >= 100)) {
+                        console.log("I am here -1")
+                        for (let x = firstuserCoupon.length; x > 2; x++) {
+                            firstuserCoupon.pop()
+                        }
+                        for (let x = 0; x < firstuserCoupon.length; x++) {
+                            // console.log("I am here")
+                            let datetoday = moment().format('DD-MM-YYYY')
+                            let b = datetoday.split("-")
+                            let zero = "0";
+                            let c = (Number(b[0]) + 7).toString()
+                            let enddate = `${c.length > 1 ? c : zero.concat(c)}-${b[1]}-${b[2]}`
+                            let valid = "1";
+                            let firstLot = new userCoupon({
+                                userid: "TidWCS",
+                                couponcode: firstuserCoupon[x].couponcode,
+                                category: firstuserCoupon[x].category,
+                                enddate: enddate,
+                                valid: valid
+                            })
+                            firstLot.save((error, couponData) => {
+                                if (error) {
+                                    logger.error('error while saving coupon data for user', 'couponToUser:userCouponDisribution()', 1)
+                                    let response = api.apiresponse(true, 403, 'error while saving coupon data for user', null)
+                                    reject(response)
+                                } else if (emptyCheck.emptyCheck(couponData)) {
+                                    logger.error('error blank data while saving coupon data for user', 'couponToUser:userCouponDisribution()', 1)
+                                    let response = api.apiresponse(true, 404, 'error blank data while saving coupon data for user', null)
+                                    reject(response)
+                                } else {
+                                    logger.info('data saved for coupon data', 'couponToUser:userCouponDisribution()')
+                                    resolve(couponData)
+                                }
+                            })
+                        }
+
+
+                        // resolve(firstuserCoupon)
+                    }
+                    if ( (purchasedAmount < 1000 && purchasedAmount >= 500)) {
+                        console.log("I am here - 2")
+                        for (let x = firstuserCoupon.length; x > 3; x++) {
+                            firstuserCoupon.pop()
+                        }
+                        for (let x = 0; x < firstuserCoupon.length; x++) {
+                            // console.log("I am here")
+                            let datetoday = moment().format('DD-MM-YYYY')
+                            let b = datetoday.split("-")
+                            let zero = "0";
+                            let c = (Number(b[0]) + 7).toString()
+                            let enddate = `${c.length > 1 ? c : zero.concat(c)}-${b[1]}-${b[2]}`
+                            let valid = "1";
+                            let firstLot = new userCoupon({
+                                userid: "TidWCS",
+                                couponcode: firstuserCoupon[x].couponcode,
+                                category: firstuserCoupon[x].category,
+                                enddate: enddate,
+                                valid: valid
+                            })
+                            firstLot.save((error, couponData) => {
+                                if (error) {
+                                    logger.error('error while saving coupon data for user', 'couponToUser:userCouponDisribution()', 1)
+                                    let response = api.apiresponse(true, 403, 'error while saving coupon data for user', null)
+                                    reject(response)
+                                } else if (emptyCheck.emptyCheck(couponData)) {
+                                    logger.error('error blank data while saving coupon data for user', 'couponToUser:userCouponDisribution()', 1)
+                                    let response = api.apiresponse(true, 404, 'error blank data while saving coupon data for user', null)
+                                    reject(response)
+                                } else {
+                                    logger.info('data saved for coupon data', 'couponToUser:userCouponDisribution()')
+                                    resolve(couponData)
+                                }
+                            })
+                        }
+
+
+                        // resolve(firstuserCoupon)
+                    }
+                    if (( purchasedAmount < 2000 && purchasedAmount >= 1000)) {
+                        console.log("I am here - 3")
+                        for (let x = firstuserCoupon.length; x > 4; x++) {
+                            firstuserCoupon.pop()
+                        }
+                        for (let x = 0; x < firstuserCoupon.length; x++) {
+                            // console.log("I am here")
+                            let datetoday = moment().format('DD-MM-YYYY')
+                            let b = datetoday.split("-")
+                            let zero = "0";
+                            let c = (Number(b[0]) + 7).toString()
+                            let enddate = `${c.length > 1 ? c : zero.concat(c)}-${b[1]}-${b[2]}`
+                            let valid = "1";
+                            let firstLot = new userCoupon({
+                                userid: "TidWCS",
+                                couponcode: firstuserCoupon[x].couponcode,
+                                category: firstuserCoupon[x].category,
+                                enddate: enddate,
+                                valid: valid
+                            })
+                            firstLot.save((error, couponData) => {
+                                if (error) {
+                                    logger.error('error while saving coupon data for user', 'couponToUser:userCouponDisribution()', 1)
+                                    let response = api.apiresponse(true, 403, 'error while saving coupon data for user', null)
+                                    reject(response)
+                                } else if (emptyCheck.emptyCheck(couponData)) {
+                                    logger.error('error blank data while saving coupon data for user', 'couponToUser:userCouponDisribution()', 1)
+                                    let response = api.apiresponse(true, 404, 'error blank data while saving coupon data for user', null)
+                                    reject(response)
+                                } else {
+                                    logger.info('data saved for coupon data', 'couponToUser:userCouponDisribution()')
+                                    resolve(couponData)
+                                }
+                            })
+                        }
+
+
+                        // resolve(firstuserCoupon)
+                    }
+                    if (purchasedAmount >= 2000) {
+                        console.log("I am here - 4")
+                        for (let x = firstuserCoupon.length; x > 5; x++) {
+                            firstuserCoupon.pop()
+                        }
+                        for (let x = 0; x < firstuserCoupon.length; x++) {
+                            // console.log("I am here")
+                            let datetoday = moment().format('DD-MM-YYYY')
+                            let b = datetoday.split("-")
+                            let zero = "0";
+                            let c = (Number(b[0]) + 7).toString()
+                            let enddate = `${c.length > 1 ? c : zero.concat(c)}-${b[1]}-${b[2]}`
+                            let valid = "1";
+                            let firstLot = new userCoupon({
+                                userid: "TidWCS",
+                                couponcode: firstuserCoupon[x].couponcode,
+                                category: firstuserCoupon[x].category,
+                                enddate: enddate,
+                                valid: valid
+                            })
+                            firstLot.save((error, couponData) => {
+                                if (error) {
+                                    logger.error('error while saving coupon data for user', 'couponToUser:userCouponDisribution()', 1)
+                                    let response = api.apiresponse(true, 403, 'error while saving coupon data for user', null)
+                                    reject(response)
+                                } else if (emptyCheck.emptyCheck(couponData)) {
+                                    logger.error('error blank data while saving coupon data for user', 'couponToUser:userCouponDisribution()', 1)
+                                    let response = api.apiresponse(true, 404, 'error blank data while saving coupon data for user', null)
+                                    reject(response)
+                                } else {
+                                    logger.info('data saved for coupon data', 'couponToUser:userCouponDisribution()')
+                                    resolve(couponData)
+                                }
+                            })
+                        }
+
+
+                        // resolve(firstuserCoupon)
+                    }
+                }
+                // start from here else
+            })
+            // }
+
+        })
+    }
+
+
+
+
+
+
+    addGeoCode(req, res).then(findValidCoupon).then(findCouponNeartoUser).then(sortCouponToUsercategory).then(couponToUser).then((resolve) => {
         logger.info('merchant available with valid coupon', 'findAllActiveCoupon:userCouponDisribution()')
         let response = api.apiresponse(false, 200, 'merchant available with valid coupon', resolve)
         res.send(response)
@@ -179,8 +453,6 @@ let userCouponDisribution = (req, res) => {
         let response = api.apiresponse(true, 500, 'no merchant available with valid coupon', null)
         res.send(response)
     })
-
-
 }
 
 
--- models/UserCoupon.js
+++ models/UserCoupon.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
-let userCoupon = new Schema({
+let usercoupon = new Schema({
     userid: {
         type: String
     },
     couponcode: {
-        type: Array
+        type: String
+    },
+    category: {
+        type: String
+    },
+    enddate: {
+        type: String
+    },
+    valid: {
+        type: String
     }
 })
+
+let merchantToUser = mongoose.model('coupondistribution', usercoupon)
+
+module.exports = {
+    merchantToUser: merchantToUser
+}
