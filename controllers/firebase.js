66bc73c06ced45359a9f5dd440e5f0fe105d7895
Shivang <shivangg66@gmail.com>
2020-05-29 12:12:17 +0530

    fcm patch

--- controllers/firebase.js
+++ controllers/firebase.js
@@ -12,7 +12,6 @@ let fcmpush = (title, body, token) => {
         credential: admin.credential.cert(serviceAccount),
         databaseURL: "https://freechers-3a71a.firebaseio.com"
     });
-    var registrationToken = 'AAAAKuRKyMA:APA91bEO0RhCnM-S1uCYZOtPN4cUGop4oyNcaWGxEqB3SV2Y51coke6oFq8Io5d8VPdc1KNkHIe9_si4rSMoLqS9maZLteuj2wy881722teAZtWESA_v8iTJp-xcytWMeJHcf-LHt_dz';
 
     const messaging = admin.messaging()
     var payload = {
